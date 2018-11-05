aa50bc7e6808642649d66f343e0482db56140b83
tainguyen197 <tainguyen197.ntt@gmail.com>
2018-11-05 19:35:15 +0700

    tao function nhan data realtime tu app1

--- app2/frontend/src/App.js
+++ app2/frontend/src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-import logo from './logo.svg';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+// import logo from './logo.svg';
 import './App.css';
-import { promises } from 'fs';
-import Async from 'react-promise'
+// import { promises } from 'fs';
+// import Async from 'react-promise'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.loadNewLocation = this.loadNewLocation.bind(this);
+    this.fetchPlaces = this.fetchPlaces.bind(this);
+
+  }
+
+  loadNewLocation(ts, ts_temp) {
+    fetch('http://localhost:3000/categories/lp?ts=' + ts)
+      .then(results => {
+        console.log(results.status);
+        if (results.status === 200) {
+          ts_temp = 1;
+          return results.json();
+        }
+      }).catch(function (err) {
+        console.log(err);
+      }).then(data => {
+        if (ts_temp === 1) {
+          console.log(data);
+          ts = data.return_ts;
+          ts_temp = 0;
+        }
+        this.loadNewLocation(ts);
+      })
+
+  }
 
   // Try HTML5 geolocation.
   fetchPlaces(mapProps, map) {
-    const { google } = mapProps;
-    const service = new google.maps.places.PlacesService(map);
+    //const { google } = mapProps;
+    //const service = new google.maps.places.PlacesService(map);
     console.log("onReady");
-    fetch('https://maps.googleapis.com/maps/api/geocode/json?address=khoa+hoc+tu+nhien&key=AIzaSyAoBGukMgWP82wOqAaDqkXeslb9V4jXH28')
-    .then(results =>{
-      return results.json();
-    }).then(data=>{
-      //lấy location(lag,lng)  dựa trên address
-      console.log(data.results[0].geometry.location);
-    })
+    var ts = 0;
+    var ts_temp = 0;
+    this.loadNewLocation(ts, ts_temp);
+
+    // fetch('https://maps.googleapis.com/maps/api/geocode/json?address=khoa+hoc+tu+nhien&key=AIzaSyAoBGukMgWP82wOqAaDqkXeslb9V4jXH28')
+    //   .then(results => {
+    //     return results.json();
+    //   }).then(data => {
+    //     //lấy location(lag,lng)  dựa trên address
+    //     //console.log(data.results[0].geometry.location);
+    //   })
   }
 
   mapClicked(mapProps, map, clickEvent) {
