6550b17bb35ed4de0cc341ebc7fa8198e1962c3e
huutuanhcmus <huutuan.hcmus@gmail.com>
2018-11-16 02:21:16 +0700

    HuuTuan-update tinh nang load map, sap xep trong app 3

--- app3/backend/app.js
+++ app3/backend/app.js
@@ -2,15 +2,20 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     cors = require('cors');
-
+var path = require('path');
 var categoryCtrl = require('./categoryController');
 
 var app = express();
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.render('index');
+ });
 app.use('/app3', categoryCtrl);
 
 var PORT = process.env.PORT || 3001;
